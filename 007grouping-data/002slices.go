9446a474a3d3492233409195115400791911168f
Sergey Vasin <vvs221@bk.ru>
2020-02-08 09:25:58 +0300

    added something

--- 007grouping-data/002slices.go
+++ 007grouping-data/002slices.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main()  {
-//    x := type { values }   composite literal (композитный литерал, составной)
+//    x := type { values }
+//   composite literal (композитный литерал, составной)
     x := []int{4, 5, 6, 7}
     fmt.Println(x)
 }
 
-// SLICE позволяет тебе группровать значения(VALUES) разного типа (TYPE)
+// слайс (SLICE) позволяет тебе группровать значения(VALUES)
+// разного типа (TYPE)
